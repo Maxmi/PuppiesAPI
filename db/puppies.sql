19a292821e74129c607b000ba068a7eebd6d4726
Bonnie Schulkin <bonnie@learnersguild.com>
2017-10-20 10:05:21 -0700

    Add live code review edits.

--- db/puppies.sql
+++ db/puppies.sql
@@ -4,12 +4,12 @@ CREATE DATABASE puppies;
 \c puppies
 
 CREATE TABLE pups (
-  ID SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   name VARCHAR,
-  breed VARCHAR,
-  age VARCHAR,
-  gender VARCHAR
+  breed VARCHAR, -- definitely have a separate table for referential integrity
+  age VARCHAR, -- better to do date of birth
+  gender VARCHAR -- separate table for referential integrity
 );
 
-INSERT INTO pups (name, breed, age, gender) 
+INSERT INTO pups (name, breed, age, gender)
 VALUES ('Coco', 'chihua-hua', 1, 'F');
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const queries = require('../db/queries');
+
 const router = express.Router();
 
-const db = require('../db/queries');
-
-router.get('/api/puppies', db.getAllPuppies);
-router.get('/api/puppies/:id', db.getSinglePuppy);
-router.post('/api/puppies', db.createPuppy);
-router.put('/api/puppies/:id', db.updatePuppy);
-router.delete('/api/puppies/:id', db.removePuppy);
+router.get('/api/puppies', (res, req, next) => {
+  queries.getAllPuppies().then((data) => {
+    res.status(200)
+      .json({
+        status: 'success',
+        data,
+        message: 'Retrieved ALL puppies',
+      });
+  })
+    .catch(err => next(err))
+})
+router.get('/api/puppies/:id', queries.getSinglePuppy);
+router.post('/api/puppies', queries.createPuppy);
+router.put('/api/puppies/:id', queries.updatePuppy);
+router.delete('/api/puppies/:id', queries.removePuppy);
 
 module.exports = router;
-
